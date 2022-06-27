cc1adde7c9275ba3451b96b0ac3e5ba35a2fc715
Mariano Ariel Cuartero <cuarteroma@gmail.com>
2022-06-27 15:37:14 -0300

    Primer Desafio JS CUARTERO - v2

--- index.js
+++ index.js
@@ -3,69 +3,53 @@ let valor;
 let resultado;
 let moneda;
 
- alert("Bienvenido a la web de conversión de ARS - USD, presione Aceptar para continuar")
+ alert("Bienvenido a la web de conversión de ARS - USD, presione Aceptar para continuar");
 
-while (continuar != "no") {
+ do {
 
-    moneda = prompt("Ingrese la moneda desde la cual desea realizar la conversión. (USD/ARS)").toUpperCase();
+    do { 
+        moneda = prompt("Ingrese la moneda desde la cual desea realizar la conversión. (USD/ARS)").toUpperCase();
 
-    switch (moneda) {
-        case "USD":
-            valor = parseFloat(prompt("Ingrese el valor en USD"));
+        if (moneda != "ARS" && moneda != "USD"){
+            alert("Debe introducir un valor válido");}
+        } while(moneda != "ARS" && moneda != "USD");
 
-            if (isNaN(valor)){
+          switch (moneda) {
 
-                while (isNaN(valor)){
-                    alert("Debe ingresar un número válido");
-                    valor = parseFloat(prompt("Ingrese el valor en USD"));
+            case "USD":
+            do{
+                valor = parseFloat(prompt("Ingrese el valor en USD"));
+                if (isNaN(valor)){
+                        alert("Debe ingresar un número válido");} 
+                else {
+                resultado = valor * 128.75;
+                alert(`El valor en ARS, sin incluir impuestos, es ARS ${resultado.toFixed(2)}-`);
                 }
-            
-                resultado = valor * 128.75;
-                alert(`El valor en ARS, sin incluir impuestos, es ARS ${resultado.toFixed(2)}-`)
-                
-            } else {
-                
-                resultado = valor * 128.75;
-                alert(`El valor en ARS, sin incluir impuestos, es ARS ${resultado.toFixed(2)}-`)
-        
-            }
-            
+
+            } while(isNaN(valor));
             break;
 
             case "ARS":
-                valor = parseFloat(prompt("Ingrese el valor en ARS"));
-    
-                if (isNaN(valor)){
-
-                    while (isNaN(valor)){
-                        alert("Debe ingresar un número válido");
-                        valor = parseFloat(prompt("Ingrese el valor en ARS"));
+                do{
+                    valor = parseFloat(prompt("Ingrese el valor en ARS"));
+                    if (isNaN(valor)){
+                            alert("Debe ingresar un número válido");}
+                    else {
+                    resultado = valor / 122.75;
+                    alert(`El valor en USD, sin incluir impuestos, es USD ${resultado.toFixed(2)}-`);
                     }
-                
-                    resultado = valor / 122.75;
-                    alert(`El valor en USD, sin incluir impuestos, es USD ${resultado.toFixed(2)}-`)
-                   
-                } else {
-                    
-                    resultado = valor / 122.75;
-                    alert(`El valor en USD, sin incluir impuestos, es USD ${resultado.toFixed(2)}-`)
-            
-                }
-
-            break;
-
-
-        default:
-            alert("Debe introducir un valor válido");
-    }
+                } while(isNaN(valor));
+                break;
+            }
           
             continuar = prompt("¿Desea continuar realizando conversiones? (Si/No)").toLowerCase();
 
-          if (continuar != "si" && continuar != "no"){
+            if (continuar != "si" && continuar != "no")
+            {
             while (continuar != "si" && continuar != "no"){
               continuar = prompt("Debe introducir un valor válido ¿Desea continuar? (Si/No)").toLowerCase();
             }
           }
 
-}
 
+ } while  (continuar != "no");
